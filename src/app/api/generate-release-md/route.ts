f58d3821ac54168d1feb3958890b3c900a0c920f
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-10-02 17:47:56 +0100

    fix(server): used replaceAll method instead of replace to replace all templates with user's prompts to prevent templates existing in prompt

--- src/app/api/generate-release-md/route.ts
+++ src/app/api/generate-release-md/route.ts
@@ -1,29 +1,28 @@
-import { generateText } from "ai"
-import { google } from "@ai-sdk/google"
-import { NextResponse } from "next/server"
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+import { NextResponse } from "next/server";
 import { PROMPTS } from "@/lib/prompts";
 
 export async function POST(request: Request) {
     const { version, changes } = await request.json();
 
     if (!version || !changes) {
-        return NextResponse.json({ error: "No data provided" }, { status: 400 })
+        return NextResponse.json({ error: "No data provided" }, { status: 400 });
     }
 
     const formatedPrompt = PROMPTS.releaseNote
-        .replace("{VERSION}", version)
-        .replace("{CHANGES}", changes.replaceAll("\r\n", "\n"));
+        .replaceAll("{VERSION}", version)
+        .replaceAll("{CHANGES}", changes.replaceAll("\r\n", "\n"));
 
     try {
         const result = await generateText({
             model: google("gemini-2.5-flash"),
             prompt: formatedPrompt,
-            // maxOutputTokens: 800,
-        })
+        });
 
-        return NextResponse.json({ result })
+        return NextResponse.json({ text: result.text });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
+        console.error(error);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
     }
 }
